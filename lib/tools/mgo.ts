1b9955c29eb89aae62ccdf27affad288c0b6c809
pillar <ymblender@gamil.com>
2021-04-16 10:12:36 +0800

    fix: update_readme

--- lib/tools/mgo.ts
+++ lib/tools/mgo.ts
@@ -4,6 +4,3 @@ import { env } from "./env";
 export const mgo = new MongoClient(env.mongo_url, {
   useUnifiedTopology: true,
 });
-export const gewuDb = () => mgo.db("gewu_doc");
-export const userColl = () => gewuDb().collection("user");
-export const simMessageColl = () => gewuDb().collection("sim_message");
